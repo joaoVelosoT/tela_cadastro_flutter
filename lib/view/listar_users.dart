fce6ed2a8b4e1214a7978a408e4d8f80ff5b2e06
João Veloso <141861100+joaoVelosoT@users.noreply.github.com>
2024-09-11 22:36:43 -0300

    termino da atividade

--- lib/view/listar_users.dart
+++ lib/view/listar_users.dart
@@ -8,28 +8,89 @@ class ListarUsers extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     mostrarUsers() {
+    //  mostrarUsers() {
       
-    }
+    // }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tela de Cadastro"),
+        title: const Text("Tela de Cadastro"),
       ),
-      body:  Column(
-        children: [
-          ListView.builder(
-            itemCount: users.length,
-            itemBuilder: (context, index){
-              Column(children: [
-                Text(users[index].nome)
-              ],);
-            }
-            )
+       body: 
+       
+       ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) { 
+          final user = users[index];
+          Icon iconSex;
+          if(user.sex == "boy"){
+            
+            iconSex =  Icon(Icons.boy, color: Colors.blue,size: 100);
+          }else {
+            // iconSex = Icons.girl;
+            iconSex =  Icon(Icons.girl, color: Colors.pink,size: 100);
+          }
+          return  
+          SingleChildScrollView(child: Column(children: [
+            Container(
+            
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(width: 1,color: Colors.black12)
+              
+            ),
+            child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                  
+                   Column(
+                    children: [
+                      // Icon(iconSex,color: Colors.blue,size: 100,),
+                      iconSex
+                      // Icon(Icons.girl,color: Colors.blue,size: 100,)
+                      
+                      // iconSex
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 30),
+                    child: Column(children: [
+                      Text(user.nome),
+                      Text(user.email),
+                      Text(user.telefone),
+                      Text(user.endereco)
+                    ],),
+                  )
+                ],)
+            ],),
+          )
+          ],),);
+          
 
-          // Text(users[1].nome)
-        ],
-      ),
+       })
+      // Column(
+      //   children: [
+      //     ListView.builder(
+      //       itemCount: users.length,
+      //       itemBuilder: (context, index){
+      //         final user = users[index];
+
+      //         return  Column(children: [
+      //           Row(children: [
+      //             Text(user.nome)
+      //           ],)
+      //         ],
+      //         );
+            
+      //         // final user = users[index];
+      //         // return Text(user.nome);
+      //       }
+      //       )
+
+          
+      //   ],
+      // ),
     );
   }
 }
--- lib/view/tela_de_cadastro.dart
+++ lib/view/tela_de_cadastro.dart
@@ -2,129 +2,156 @@ import 'package:flutter/material.dart';
 import 'package:tela_cadastro/models/user_model.dart';
 import 'package:tela_cadastro/view/listar_users.dart';
 
-class TelaDeCadastro extends StatelessWidget {
+class TelaDeCadastro extends StatefulWidget {
   const TelaDeCadastro({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    List<UserModel> users = [];
+  State<TelaDeCadastro> createState() => _TelaDeCadastroState();
+}
 
-    TextEditingController nomeController = TextEditingController();
-    TextEditingController emailController = TextEditingController();
-    TextEditingController telefoneController = TextEditingController();
-    TextEditingController enderecoController = TextEditingController();
+class _TelaDeCadastroState extends State<TelaDeCadastro> {
+  List<UserModel> users = [];
 
-    void salvarInfo() {
-      if (_isFemaleChecked) {
-        sex = "girl";
-      } else if (_isMaleChecked) {
-        sex = "boy";
-      } 
+  TextEditingController nomeController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController telefoneController = TextEditingController();
+  TextEditingController enderecoController = TextEditingController();
 
+  void salvarInfo() {
+    if (_isFemaleChecked) {
+      sex = "girl";
+    } else if (_isMaleChecked) {
+      sex = "boy";
+    }
+
+
+    setState(() {
       users.add(UserModel(
           nome: nomeController.text,
           email: emailController.text,
           telefone: telefoneController.text,
           endereco: enderecoController.text,
           sex: sex));
-  print(nomeController.text);
-      print(emailController.text);
-      print(telefoneController.text);
-      print(enderecoController.text);
-      print(sex);
-          nomeController.text = "";
-          emailController.text = "";
-          telefoneController.text = "";
-          enderecoController.text = "";
-          _isFemaleChecked = false;
-          _isMaleChecked = false;
-      
-    }
 
+      nomeController.text = "";
+      emailController.text = "";
+      telefoneController.text = "";
+      enderecoController.text = "";
+
+      _isFemaleChecked = false;
+      _isMaleChecked = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Tela de Cadastro"),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: TextField(
-              controller: nomeController,
-              decoration: const InputDecoration(
-                  label: Center(child: Text("Nome")),
-                  border: OutlineInputBorder()),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: TextField(
+                controller: nomeController,
+                decoration: const InputDecoration(
+                    label: Center(child: Text("Nome")),
+                    border: OutlineInputBorder()),
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(18.0),
-            child: TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                  label: Center(child: Text("Email")),
-                  border: OutlineInputBorder()),
+            Padding(
+              padding: EdgeInsets.all(18.0),
+              child: TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                    label: Center(child: Text("Email")),
+                    border: OutlineInputBorder()),
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(18.0),
-            child: TextField(
-              controller: telefoneController,
-              decoration: const InputDecoration(
-                  label: Center(child: Text("Telefone")),
-                  border: OutlineInputBorder()),
+            Padding(
+              padding: EdgeInsets.all(18.0),
+              child: TextField(
+                controller: telefoneController,
+                decoration: const InputDecoration(
+                    label: Center(child: Text("Telefone")),
+                    border: OutlineInputBorder()),
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(18.0),
-            child: TextField(
-              controller: enderecoController,
-              decoration: const InputDecoration(
-                  label: Center(child: Text("Endereço")),
-                  border: OutlineInputBorder()),
+            Padding(
+              padding: EdgeInsets.all(18.0),
+              child: TextField(
+                controller: enderecoController,
+                decoration: const InputDecoration(
+                    label: Center(child: Text("Endereço")),
+                    border: OutlineInputBorder()),
+              ),
             ),
-          ),
-          GenderCheckboxes(),
-          ElevatedButton(
-              onPressed: () {
-                // funcao de salvar
-                salvarInfo();
-                 ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: const Text('Salvo com sucesso!'),
-                          action: SnackBarAction(
-                            label: 'Fechar',
-                            onPressed: () {
-                              // Code to execute.
-                            },
-                          ),
+            GenderCheckboxes(),
+            ElevatedButton(
+                onPressed: () {
+                  // funcao de salvar
+
+                  if (nomeController.text.isEmpty ||
+                      emailController.text.isEmpty ||
+                      telefoneController.text.isEmpty ||
+                      enderecoController.text.isEmpty ||
+                      !_isFemaleChecked && !_isMaleChecked) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: const Text('Revise os dados!'),
+                        action: SnackBarAction(
+                          label: 'Fechar',
+                          onPressed: () {
+                            // Code to execute.
+                          },
                         ),
-                      );
-              },
-              child: Text("Salvar")),
-          ElevatedButton(
-              onPressed: () {
-                // funcao
-                if(users.isEmpty){
-                   ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: const Text('Adicione um primeiro!'),
-                          action: SnackBarAction(
-                            label: 'Fechar',
-                            onPressed: () {
-                              // Code to execute.
-                            },
-                          ),
+                      ),
+                    );
+                  } else {
+                    salvarInfo();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: const Text('Salvo com sucesso!'),
+                        action: SnackBarAction(
+                          label: 'Fechar',
+                          onPressed: () {
+                            // Code to execute.
+                          },
                         ),
-                      );
-                }else {
-                  Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => ListarUsers(users: users,)
-                ));
-                }
-                
-              },
-              child: Text("Ver Usuarios"))
-        ],
+                      ),
+                    );
+                  }
+                },
+                child: Text("Salvar")),
+            ElevatedButton(
+                onPressed: () {
+                  // funcao
+                  if (users.isEmpty) {
+                    print("users teste" + users.length.toString());
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: const Text('Adicione um primeiro!'),
+                        action: SnackBarAction(
+                          label: 'Fechar',
+                          onPressed: () {
+                            // Code to execute.
+                          },
+                        ),
+                      ),
+                    );
+                  } else {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ListarUsers(
+                              users: users,
+                            )));
+                  }
+                },
+                child: Text("Ver Usuarios"))
+          ],
+        ),
       ),
     );
   }
@@ -156,19 +183,17 @@ class _GenderCheckboxesState extends State<GenderCheckboxes> {
                   // Garantir que apenas um checkbox pode ser selecionado por vez
                   if (_isMaleChecked) {
                     _isFemaleChecked = false;
-                    
                   }
                 });
               },
             ),
-            Text('Masculino'),
+            const Text('Masculino'),
           ],
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Checkbox(
-              
               value: _isFemaleChecked,
               onChanged: (bool? value) {
                 setState(() {
@@ -176,15 +201,14 @@ class _GenderCheckboxesState extends State<GenderCheckboxes> {
                   // Garantir que apenas um checkbox pode ser selecionado por vez
                   if (_isFemaleChecked) {
                     _isMaleChecked = false;
-                   
                   }
                 });
               },
             ),
-            Text('Feminino'),
+            const Text('Feminino'),
           ],
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
       ],
     );
   }
